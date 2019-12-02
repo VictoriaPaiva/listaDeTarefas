5f8dd5828898ab3dcbf871fcc631a352fb3e95a3
VictoriaPaiva <57760051+VictoriaPaiva@users.noreply.github.com>
2019-12-02 10:23:33 -0300

    lista de tarefas

---
+++ listaDeTarefas/assets/lista.js.js
@@ -0,0 +1,7 @@
+;
+function checkList()
+{
+    var itemLista = document.getElementById("tarefa").value;
+    document.getElementById("coisa").innerHTML+='<div class="row"><div class="col-md-2"><input type="checkbox" id="check"> </div><div>'+ itemLista+'</div><div class="col-md-2">num();</div></div> '
+    
+}//<div class ="col-md-8" id="listaTarefas">'+document.getElementById("tarefa").value;+'</div><div class="col-md-2">num();</div></div> '
